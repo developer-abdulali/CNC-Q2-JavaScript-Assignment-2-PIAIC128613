c9bb3b0ccb2d3876c7bbae1eb2507f128a64459d
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-02-15 23:16:20 +0500

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,27 @@
+// var a = 4;
+// if (++a === 5) {
+//    alert("given condition for variable a is true");  // true
+// }
+
+// var b = 82;
+// if (b++ === 83){
+//     alert("given condition for variable b is true");  // false
+// }
+
+// var c = 12;
+
+// if (c++ === 13){
+// alert("condition 1 is true");   // false
+// }
+
+// if (c === 13){
+// alert("condition 2 is true");   // false
+// }
+
+// if (++c < 14){
+// alert("condition 3 is true");   // true
+// }
+
+// if(c === 14){
+// alert("condition 4 is true");   // false
+// }
