1f8952dd38a43004eb89c5c63fe57f8955a9c479
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-02-16 18:14:28 +0500

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,9 @@
+// Prompt the user to enter a number
+let number = +prompt("Enter a number: ");
+
+// Check if the number is even or odd
+if (number % 2 === 0) {
+  document.write(number + " is an even number.");
+} else {
+  document.write(number + " is an odd number.");
+}
