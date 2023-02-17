b9675fc4a3c387ffeb2ab5945adf25dc45d6c444
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-02-17 16:48:18 +0500

    Add files via upload

---
+++ task8.js
@@ -0,0 +1,11 @@
+// Prompt the user to enter a number
+var number = +prompt("Enter a number: ");
+
+// Check whether the number is positive, negative, or zero
+if (number > 0) {
+  document.write("The number is positive");
+} else if (number < 0) {
+  document.write("The number is negative");
+} else {
+  document.write("The number is zero");
+}
