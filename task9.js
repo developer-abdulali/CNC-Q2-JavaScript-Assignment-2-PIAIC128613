23b99f42686f7768326d87f0ae4f1b1ae3e92cde
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-02-25 16:34:28 +0500

    Add files via upload

---
+++ task9.js
@@ -0,0 +1,8 @@
+var userInput = prompt('Enter your charactor');
+
+if(userInput === 'a' || userInput === 'e' || userInput === 'i' || userInput === 'o' ||userInput === 'u'){
+    document.write('true');
+}
+else{
+    document.write('false');
+}
