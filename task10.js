2e15cf531c51eba2c25afd015fdebcb63a3d39bb
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-02-25 16:48:01 +0500

    Add files via upload

---
+++ task10.js
@@ -0,0 +1,8 @@
+var password = 'javascript';
+var userPassword = prompt('Enter your password');
+
+if(userPassword === password){
+    alert('Correct!');
+} else{
+    alert('Inccorect Password');
+}
